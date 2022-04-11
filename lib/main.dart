5382255878d9c0514a8fbc03fa3047d033e5b82a
arsalan manzoor <arsalan1998m@gmail.com>
2022-04-11 23:44:37 +0500

    centering row/column items

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,6 +7,8 @@ void main() {
 }
 
 // === Row === //
+// height: double.inifinity means take full available screen height
+// width: double.infinity means take full available screen width
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,16 +16,21 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('1st Row'),
-              SizedBox(
-                width: 10,
-              ),
-              Text('2nd Row'),
-              Text('3rd Row'),
-            ],
+          body: Container(
+            // width: double.infinity,
+            height: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text('1st Row'),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('2nd Row'),
+                Text('3rd Row'),
+              ],
+            ),
           ),
         ),
       ),
