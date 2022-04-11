2f6c672e4fe8c5b7ee68fdf6301341cb2e6ee8ed
arsalan manzoor <arsalan1998m@gmail.com>
2022-04-11 23:33:33 +0500

    row widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,26 @@ void main() {
   runApp(MyApp());
 }
 
-// === Column === //
-
-// === SizedBox ===  //
-// Used For Spacing
-// if(using column then use height)
-// if(using Row then use width)
-
+// === Row === //
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-            body: Column(
-          children: <Widget>[
-            Text('1st Column'),
-            SizedBox(
-              height: 10,
-            ),
-            Text('2nd Column'),
-            Text('3rd Column'),
-            SizedBox(
-              height: 10,
-            ),
-            Text('4th Column'),
-            Text('5th Column'),
-          ],
-        )),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('1st Row'),
+              SizedBox(
+                width: 10,
+              ),
+              Text('2nd Row'),
+              Text('3rd Row'),
+            ],
+          ),
+        ),
       ),
     );
   }
