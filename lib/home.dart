19ecc7cc1296058c36f842bc9489f08050d0135d
arsalan manzoor <arsalan1998m@gmail.com>
2022-04-12 01:10:15 +0500

    stack widget

--- lib/home.dart
+++ lib/home.dart
@@ -7,33 +7,55 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-// Size ky liyeh hum circle avatar mein radius dyty hain
-
-// Network Image Example
-// class _HomeState extends State<Home> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: CircleAvatar(
-//         radius: 60,
-//         backgroundImage: NetworkImage(
-//             "https://avatars.githubusercontent.com/u/44143816?v=4"),
-//       ),
-//     );
-//   }
-// }
-
-// Asset Image Example
+// Stack is like our html/css position element
+// Positioned is only used in Stack Widget
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CircleAvatar(
-        radius: 160,
-        backgroundImage: AssetImage(
-          "assets/images/login-background.jpg",
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+          ),
         ),
-      ),
+        Positioned(
+          left: 20,
+          top: 20,
+          child: Container(
+            color: Colors.black,
+            width: 100,
+            height: 100,
+          ),
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+            color: Colors.blue,
+            width: 100,
+            height: 100,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            color: Colors.yellow,
+            width: 100,
+            height: 100,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            color: Colors.green,
+            width: 100,
+            height: 100,
+          ),
+        ),
+      ],
     );
   }
 }
