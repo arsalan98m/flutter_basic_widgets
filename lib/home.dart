7da15e8235d368a492ba74e8e34da2b41c5fa446
arsalan manzoor <arsalan1998m@gmail.com>
2022-04-12 01:14:40 +0500

    responsiveness

--- lib/home.dart
+++ lib/home.dart
@@ -7,55 +7,13 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-// Stack is like our html/css position element
-// Positioned is only used in Stack Widget
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.topLeft,
-          child: Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-          ),
-        ),
-        Positioned(
-          left: 20,
-          top: 20,
-          child: Container(
-            color: Colors.black,
-            width: 100,
-            height: 100,
-          ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(
-            color: Colors.blue,
-            width: 100,
-            height: 100,
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-            color: Colors.yellow,
-            width: 100,
-            height: 100,
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            color: Colors.green,
-            width: 100,
-            height: 100,
-          ),
-        ),
-      ],
+    return Container(
+      color: Colors.pink,
+      width: MediaQuery.of(context).size.width * 0.2,
+      height: MediaQuery.of(context).size.height * 0.2,
     );
   }
 }
