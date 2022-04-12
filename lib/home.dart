20c6b4f7f07934570f01fcc53e8b54cf44391a0d
arsalan manzoor <arsalan1998m@gmail.com>
2022-04-12 12:07:57 +0500

    learning stack widget

--- lib/home.dart
+++ lib/home.dart
@@ -9,51 +9,138 @@ class Home extends StatefulWidget {
 
 // Stack is like our html/css position element
 // Positioned is only used in Stack Widget
+// class _HomeState extends State<Home> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         Align(
+//           alignment: Alignment.topLeft,
+//           child: Container(
+//             color: Colors.red,
+//             width: 100,
+//             height: 100,
+//           ),
+//         ),
+//         Positioned(
+//           left: 20,
+//           top: 20,
+//           child: Container(
+//             color: Colors.black,
+//             width: 100,
+//             height: 100,
+//           ),
+//         ),
+//         Align(
+//           alignment: Alignment.topRight,
+//           child: Container(
+//             color: Colors.blue,
+//             width: 100,
+//             height: 100,
+//           ),
+//         ),
+//         Align(
+//           alignment: Alignment.bottomLeft,
+//           child: Container(
+//             color: Colors.yellow,
+//             width: 100,
+//             height: 100,
+//           ),
+//         ),
+//         Align(
+//           alignment: Alignment.bottomRight,
+//           child: Container(
+//             color: Colors.green,
+//             width: 100,
+//             height: 100,
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Align(
-          alignment: Alignment.topLeft,
-          child: Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
+        Container(
+          color: Color(
+            0xffe0e0e,
           ),
         ),
-        Positioned(
-          left: 20,
-          top: 20,
+        Padding(
+          padding: const EdgeInsets.only(bottom: 100),
           child: Container(
-            color: Colors.black,
-            width: 100,
-            height: 100,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+              ),
+              color: Colors.white,
+            ),
           ),
         ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(
-            color: Colors.blue,
-            width: 100,
-            height: 100,
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-            color: Colors.yellow,
-            width: 100,
-            height: 100,
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            color: Colors.green,
-            width: 100,
-            height: 100,
-          ),
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 300,
+              decoration: const BoxDecoration(
+                color: Color(0xff123456),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Icon(Icons.menu_outlined,
+                            size: 40, color: Colors.white),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Icon(Icons.more_horiz,
+                            size: 40, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 18.0),
+                      child: Text(
+                        'Welcome',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: -40,
+              left: 30,
+              // bottom: -40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(1000.0),
+                child: Image.network(
+                  "https://avatars.githubusercontent.com/u/44143816?v=4",
+                  scale: 3.8,
+                ),
+              ),
+            )
+          ],
         ),
       ],
     );
